045fc03147b50855d66d2d522a6cf3db4b16ad38
Aleksey-Kon <windowsviser@gmail.com>
2024-05-31 12:24:26 +0300

    Initial commit

---
+++ WpfApp16/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Data.SQLite;
+
+namespace WpfApp16
+{
+    public class User {
+        public int ID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronomic { get; set; }
+        public string Birthday { get; set; }
+        public string Place { get; set; }
+        public string Number { get; set; }
+        public string Age { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private static string DBFile;
+        private static SQLiteConnection Sqlcon;
+        private static SQLiteCommand SQLCommand;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AutoSelect();
+        }
+
+        public void AutoSelect()
+        {
+            DBFile = "MainDB.db";
+            List<User> users = new List<User>();
+
+            Sqlcon = new SQLiteConnection();
+            SQLCommand = new SQLiteCommand();
+            Sqlcon = new SQLiteConnection("Data Source=MainDB.db;Version=3;");
+            Sqlcon.Open();
+            SQLiteDataReader sqlData;
+            SQLCommand = Sqlcon.CreateCommand();
+            SQLCommand.CommandText = "Select * From TestTable";
+            sqlData = SQLCommand.ExecuteReader();
+            while(sqlData.Read())
+            {
+                users.Add(new User() { ID = Convert.ToInt32(sqlData["id"]), Surname = sqlData["Surname"].ToString(), 
+                    Name = sqlData["Name"].ToString(), Patronomic = sqlData["Patronomic"].ToString(),
+                    Birthday = sqlData["Birthday"].ToString(), Place = sqlData["Place"].ToString(),
+                    Number = sqlData["Number"].ToString(), Age = sqlData["Age"].ToString() });
+            }
+            MainDG.ItemsSource = users;
+            Sqlcon.Close();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow SW = new SettingsWindow();
+            SW.Show();
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            AutoSelect();
+        }
+    }
+}
+
+
---
+++ WpfApp16/SettingsWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp16
+{
+    public partial class SettingsWindow : Window
+    {
+        private string DBFile;
+        private SQLiteConnection Sqlcon;
+        private SQLiteCommand SQLCommand;
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DBFile = "MainDB.db";
+            List<User> users = new List<User>();
+
+            Sqlcon = new SQLiteConnection();
+            SQLCommand = new SQLiteCommand();
+            Sqlcon = new SQLiteConnection("Data Source=MainDB.db;Version=3;");
+            Sqlcon.Open();
+            SQLCommand = Sqlcon.CreateCommand();
+            SQLCommand.CommandText = "Insert into TestTable (Surname, Name, Patronomic, Birthday, Place, Number, Age) values" +
+                "('"+Tb0.Text+ "', '"+Tb1.Text+ "', '"+Tb2.Text+ "', '"+Tb3.Text+ "', '"+Tb4.Text+ "', '"+Tb5.Text+ "', '"+Tb6.Text+ "')";
+            SQLCommand.ExecuteNonQuery();
+            
+            Sqlcon.Close();
+            MessageBox.Show("Данные записаны");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            DBFile = "MainDB.db";
+            List<User> users = new List<User>();
+
+            Sqlcon = new SQLiteConnection();
+            SQLCommand = new SQLiteCommand();
+            Sqlcon = new SQLiteConnection("Data Source=MainDB.db;Version=3;");
+            Sqlcon.Open();
+            SQLCommand = Sqlcon.CreateCommand();
+            SQLCommand.CommandText = "Delete from TestTable where id like '"+Tb7.Text+"'";
+            SQLCommand.ExecuteNonQuery();
+
+            Sqlcon.Close();
+            MessageBox.Show("Данные удалены");
+        }
+    }
+}
